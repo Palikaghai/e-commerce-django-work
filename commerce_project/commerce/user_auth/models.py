790086e343f6edc1954b09d5f58c0d9fe214b4d4
Palika Ghai <149198562+Palikaghai@users.noreply.github.com>
2026-02-09 10:14:50 +0530

    Update models.py

--- commerce_project/commerce/user_auth/models.py
+++ commerce_project/commerce/user_auth/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, related_name='profile')
     phone = models.CharField(max_length=30, blank=True)
     country = models.CharField(max_length=100, blank=True)
 
